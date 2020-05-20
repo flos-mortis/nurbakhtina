d698e4fc6f6b4495cab70c62e396a874556a77b9
Sylu <liccy.sy@yandex.ru>
2020-05-21 00:46:29 +0500

    introduction

---
+++ silu/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace silu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I, Silu Nurbakhtina, have a dream! " +
+                "I want to become a famous web-developer and live in Silicon Valley." +
+                "Also I'm good at drawing, curious about new things and try my best in programming!");
+        }
+    }
+}
